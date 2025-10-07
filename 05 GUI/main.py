31077fb53d839530066418e08e6de96551c0e9e0
GeraltOfRiviaa <samuelsvob@gmail.com>
2025-10-07 13:48:22 +0200

    Merge branch 'main' of https://github.com/GeraltOfRiviaa/PythonPVY

--- 05 GUI/main.py
+++ 05 GUI/main.py
@@ -1,16 +1,55 @@
 import tkinter as tk
+import random
 
 class App:
     def __init__(self):
         self.root = tk.Tk()
         self.root.title("Demo GUI")
         self.root.geometry("650x450")
+        self.random = random.Random()
+
+        self.canvas = tk.Canvas(self.root, width=650, height=450, bg="white")
+        self.canvas.pack(fill="both",expand=True)
+        self.canvas.delete
+
         menu_bar = tk.Menu(self.root)
         self.root.config(menu=menu_bar)
+
         m_tasks = tk.Menu(menu_bar, tearoff=False)
         m_tasks.add_command(label="Exit", command=self.root.destroy)
         menu_bar.add_cascade(label="System", menu=m_tasks)
 
+        m_graphics = tk.Menu(menu_bar, tearoff=False)
+        m_graphics.add_command(label="Magical lines", command=lambda: self.magic_barcode(2,5))
+        m_graphics.add_command(label="Checkerboard", command=self.root.destroy)
+        m_graphics.add_command(label="Target", command=self.root.destroy)
+        menu_bar.add_cascade(label="Graphics", menu=m_graphics)
+
+        m_colors = tk.Menu(menu_bar, tearoff=False)
+        m_colors.add_command(label="Red", command=lambda: self.set_bg("red"))
+        m_colors.add_command(label="Green",command=lambda: self.set_bg("green"))
+        m_colors.add_command(label="Blue",command=lambda: self.set_bg("blue"))
+        m_colors.add_command(label="Clear",command=lambda: self.clear_bg)
+        menu_bar.add_cascade(label="Colors", menu=m_colors)
+
+    def set_bg(self, color:str) -> None:
+        self.canvas.config(bg=color)
+    def clear_bg(self) -> None:
+        self.canvas.delete("all")
+
+    def magic_barcode(self, min:int, max:int) -> None:
+        self.clear_bg()
+        self.canvas.config(bg="white")
+        
+        
+        x = 0
+        while x < 800:
+            
+            width = self.random.randint(min, max)
+            color = f"#{random.randint(0,0xffffff):06x}"
+            self.canvas.create_line(x,0,x,600, fill=color, width=width)
+            x += width
+        
     def run(self):
         self.root.mainloop()
 
