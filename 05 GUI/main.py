e0c427a052cf258b5296238a460a165e9d4ce417
GeraltOfRiviaa <samuelsvob@gmail.com>
2025-10-16 08:02:42 +0200

    another lesson

--- 05 GUI/main.py
+++ 05 GUI/main.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 import random
+from tkinter import simpledialog
+from tkinter import colorchooser
 
 class App:
     def __init__(self):
@@ -11,6 +13,7 @@ class App:
         self.canvas = tk.Canvas(self.root, width=650, height=450, bg="white")
         self.canvas.pack(fill="both",expand=True)
         self.canvas.delete
+        width,height = self._get_canvas_size()
 
         menu_bar = tk.Menu(self.root)
         self.root.config(menu=menu_bar)
@@ -21,7 +24,7 @@ class App:
 
         m_graphics = tk.Menu(menu_bar, tearoff=False)
         m_graphics.add_command(label="Magical lines", command=lambda: self.magic_barcode(2,5))
-        m_graphics.add_command(label="Checkerboard", command=self.root.destroy)
+        m_graphics.add_command(label="Checkerboard", command=lambda: self.checkerboard_dialog())
         m_graphics.add_command(label="Target", command=self.root.destroy)
         menu_bar.add_cascade(label="Graphics", menu=m_graphics)
 
@@ -29,6 +32,7 @@ class App:
         m_colors.add_command(label="Red", command=lambda: self.set_bg("red"))
         m_colors.add_command(label="Green",command=lambda: self.set_bg("green"))
         m_colors.add_command(label="Blue",command=lambda: self.set_bg("blue"))
+        
         m_colors.add_command(label="Clear",command=lambda: self.clear_bg)
         menu_bar.add_cascade(label="Colors", menu=m_colors)
 
@@ -43,7 +47,7 @@ class App:
         w,h, = self._get_canvas_size()
         
         x = 0
-        while x < 800:
+        while x < w:
             
             width = self.random.randint(min, max)
             color = f"#{random.randint(0,0xffffff):06x}"
@@ -54,6 +58,26 @@ class App:
         w = self.canvas.winfo_width() or self.root.winfo_width or 600
         h = self.canvas.winfo_height() or self.root.winfo_height
         return w,h    
+    
+    def checkerboard(self, size: int = 8, light: str = "#eee" , dark: str = "#111") -> None:
+        self.clear_bg()
+        self.canvas.config(bg=light)
+        w,h = self._get_canvas_size()
+        
+        cell_x = w / size
+        cell_y = h / size
+
+        for y in range(size):
+            for x in range(size):
+                if (x + y) % 2 == 0:
+                    self.canvas.create_rectangle(x*cell_x,y * cell_y,x*cell_x+cell_x, y * cell_y * cell_y,
+                                                  fill=dark,outline="#333", width=5)
+    def checkerboard_dialog(self):
+        size = simpledialog.askinteger(title="Chessboard", prompt="Enter the size of checker board on x axes",
+                                       minvalue=2,maxvalue=20,initialvalue=8,parent=self.root)
+        
+        light = colorchooser.askcolor(title="Choose a color of background: ")[1]
+        self.checkerboard(size=size, light=light)
     def run(self):
         self.root.mainloop()
 
